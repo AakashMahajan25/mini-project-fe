2993efb3fc7d68b91a49d8fe9e44486ff8e8fa69
Shubham Patel <shubham@airrchip.com>
2024-01-23 19:04:11 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.scss';
+import Dashboard from './screens/dashboard/Dashboard';
+
+function App() {
+  return (
+    <>
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
