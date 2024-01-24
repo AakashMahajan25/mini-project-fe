685d1db9d042c009104b13eec1704bdbc13e0d31
Shubham Patel <shubham@airrchip.com>
2024-01-24 19:52:37 +0530

    row

--- src/screens/dashboard/Dashboard.js
+++ src/screens/dashboard/Dashboard.js
@@ -2,13 +2,26 @@ import React from 'react'
 import TopBar from '../../components/topBar/TopBar'
 import StockPriceScroll from '../../components/stockPriceScroll/StockPriceScroll'
 import LeftBox from '../../components/leftBox/LeftBox'
+import DashboardRightBox from '../../components/dashboardRightBox/DashboardRightBox'
 
 function Dashboard() {
     return (
         <>
             <TopBar />
             <StockPriceScroll />
-            <LeftBox />
+            <div className='row justify-content-between m-0'>
+                <div className='col-lg-3'>
+                    <LeftBox />
+                </div>
+                <div className='col-lg-6'>
+                    <p>shubham</p>
+                    <p>shubham</p>
+                    <p>shubham</p>
+                </div>
+                <div className='col-lg-3'>
+                    <DashboardRightBox />
+                </div>
+            </div>
         </>
     )
 }
