79d03cfb0935dab3a0cc9794ad553b8a03ebb40b
Govind Shah <govind@airrchip.com>
2024-02-15 13:37:08 +0530

    api changed

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://13.126.207.249:3000/api/', // Your API base URL
+  baseURL: 'https://api.frruit.co/api/',
+  // baseURL: 'http://13.126.207.249:3000/api/', // Your API base URL
   // baseURL: "http://localhost:4000/api/", // Your Local API base URL
   headers: {
     "Content-Type": "application/json",
