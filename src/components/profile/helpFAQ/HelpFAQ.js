d6228a98ffc51f82b4e6cb0f46bb9e55efcb3af2
Shubham Patel <shubham@airrchip.com>
2024-02-28 17:52:13 +0530

    chnages

--- src/components/profile/helpFAQ/HelpFAQ.js
+++ src/components/profile/helpFAQ/HelpFAQ.js
@@ -4,6 +4,8 @@ import Support from '../../../assets/images/support.png'
 import MailIcon from '../../../assets/images/blue-mail-img.png'
 import PhoneIcon from '../../../assets/images/blue-phone-img.png'
 import SearchIcon from '../../../assets/images/search-icon.png';
+import FAQImg from '../../../assets/images/faq-img.png';
+import Accordion from 'react-bootstrap/Accordion';
 
 function HelpFAQ() {
     return (
@@ -33,20 +35,54 @@ function HelpFAQ() {
                     </div>
                 </div>
                 <div className='gray-boder'></div>
+
                 <div className='faq-section'>
-                    <div className='faq-text'>Frequently Asked Question</div>
-                    {/* <div className="position-relative" style={{ marginBottom: 10}}>
-                        <input type="text" className="form-control form-control-search" placeholder='Search Here' />
-                        <div className="position-absolute" style={{ left: 31, top: '15%' }}>
-                            <img src={SearchIcon} style={{ width: 20, objectFit: 'contain', cursor: 'pointer' }} alt="Search Icon" />
+                    <div className='row'>
+                        <div className='col-lg-6 col-sm-12'>
+                            <div className='faq-text'>Frequently Asked Question</div>
+                            <div className='searchbar-css'>
+                                <div className="position-relative mt-3" style={{ marginBottom: 20 }}>
+                                    <input type="text" style={{ backgroundColor: 'white' }} className="form-control form-control-search" placeholder='Search Preferences' />
+                                    <div className="position-absolute" style={{ left: 15, top: '15%' }}>
+                                        <img src={SearchIcon} style={{ width: 20, objectFit: 'contain', cursor: 'pointer' }} alt="Search Icon" />
+                                    </div>
+                                </div>
+                            </div>
+                            <div>
+                                <Accordion>
+                                    <Accordion.Item eventKey="0">
+                                        <Accordion.Header>Does Frruit give Financial Advices?</Accordion.Header>
+                                        <Accordion.Body>
+                                            While Frruit serves as a robust investment research assistant, it's crucial to understand it doesn't offer financial advice. Instead, Frruit provides comprehensive market data, insights, and analysis, empowering you to draw your own conclusions about investments. While Frruit enhances your investment know-how, it's essential not to depend solely on it for making financial decisions.
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+                                    <Accordion.Item eventKey="1">
+                                        <Accordion.Header>Does Frruit give Financial Advices?</Accordion.Header>
+                                        <Accordion.Body>
+                                            While Frruit serves as a robust investment research assistant, it's crucial to understand it doesn't offer financial advice. Instead, Frruit provides comprehensive market data, insights, and analysis, empowering you to draw your own conclusions about investments. While Frruit enhances your investment know-how, it's essential not to depend solely on it for making financial decisions.
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+                                    <Accordion.Item eventKey="2">
+                                        <Accordion.Header>Does Frruit give Financial Advices?</Accordion.Header>
+                                        <Accordion.Body>
+                                            While Frruit serves as a robust investment research assistant, it's crucial to understand it doesn't offer financial advice. Instead, Frruit provides comprehensive market data, insights, and analysis, empowering you to draw your own conclusions about investments. While Frruit enhances your investment know-how, it's essential not to depend solely on it for making financial decisions.
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+                                    <Accordion.Item eventKey="3">
+                                        <Accordion.Header>Does Frruit give Financial Advices?</Accordion.Header>
+                                        <Accordion.Body>
+                                            While Frruit serves as a robust investment research assistant, it's crucial to understand it doesn't offer financial advice. Instead, Frruit provides comprehensive market data, insights, and analysis, empowering you to draw your own conclusions about investments. While Frruit enhances your investment know-how, it's essential not to depend solely on it for making financial decisions.
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+                                </Accordion>
+                            </div>
                         </div>
-                    </div> */}
-                    <div className="position-relative mt-3" style={{ marginBottom: 20 }}>
-                    <input type="text" style={{ backgroundColor: 'white' }} className="form-control form-control-search" placeholder='Search Preferences' />
-                    <div className="position-absolute" style={{ left: 15, top: '15%' }}>
-                        <img src={SearchIcon} style={{ width: 20, objectFit: 'contain', cursor: 'pointer' }} alt="Search Icon" />
+                        <div className='col-lg-6 col-sm-12'>
+                            <div className='d-flex align-items-center justify-content-center' style={{ height: window.innerHeight - 350 }}>
+                            <img src={FAQImg} width={300} style={{objectFit:'contain'}} />
+                            </div>
+                        </div>
                     </div>
-                </div>
                 </div>
             </section>
         </>
