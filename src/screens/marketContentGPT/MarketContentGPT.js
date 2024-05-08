3cbc426c9deea8704728d582af6dbefaa8e0fc6b
Zaheer Shaikh <zaheer@airrchip.com>
2024-05-08 16:36:19 +0530

    changes

--- src/screens/marketContentGPT/MarketContentGPT.js
+++ src/screens/marketContentGPT/MarketContentGPT.js
@@ -136,6 +136,7 @@ function MarketContentGPT() {
         } catch (error) {
             clearQuestionAndToastError(error);
             setSelectedFile(null)
+            setShowQuestion(false);  
         }
     }
 
