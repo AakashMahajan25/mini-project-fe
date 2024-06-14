2d49a4ebb08dd83f496c862a9f8579349b890777
Viral Borana <viral@airrchip.com>
2024-06-14 18:15:56 +0530

    ui changes

--- src/components/promptsLibrary/PromptsLibrary.js
+++ src/components/promptsLibrary/PromptsLibrary.js
@@ -50,13 +50,13 @@ function PromptsLibrary(props) {
     
     return (
         <div className='promptButton'>
-            {!show && <button onClick={() => setShow(!show)} className='blue-btn radius-button d-flex align-items-center justify-content-center' ><img src={RightWhiteArrow} style={{ width: 8, height: 13, objectFit: 'contain', marginRight: 5 }} /> PromptsLibrary</button>}
+            {!show && <button onClick={() => setShow(!show)} className='blue-btn radius-button d-flex align-items-center justify-content-center' ><img src={RightWhiteArrow} className='prompt-button-img' />Prompts Library</button>}
 
 
             {show && <div className='promptView' style={{animation: show ? 'slideInRight 0.3s ease-in-out' : 'none'}}>
                 <button onClick={() => setShow(!show)} className='blue-btn radius-small-button d-flex align-items-center justify-content-center' style={{ height: 40 }} ><img src={LeftWhiteArrow} style={{ width: 8, height: 13, objectFit: 'contain' }} /></button>
 
-                <div className='showPrompt' style={{ height: window.innerHeight - 68 }}>
+                <div className='showPrompt' style={ window.innerWidth > 500 ? { height: window.innerHeight - 68 } : { height: window.innerHeight - 91}}>
                     <h4 className='title'>Prompts Library</h4>
                     <div >
                         <Tab.Container defaultActiveKey={type} onSelect={(e)=>handleTab(e)}>
