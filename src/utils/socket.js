c59c5efbccaaf09169e07ebb619a0d61009ce6e4
Govind Shah <govind@airrchip.com>
2024-09-16 16:56:33 +0530

    port removed from socket

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const URL = "http://18.233.54.54:3000/chat";
+const URL = "http://18.233.54.54/chat";
 
 export const socket = io(URL, {
     autoConnect: false,
